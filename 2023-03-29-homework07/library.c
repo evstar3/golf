909332e2568d8ca0a0dd1e32adf03c5c58c5429b
Evan Day <evanday19@gmail.com>
2023-03-30 15:51:37 -0400

    golfed further by reusing flags string as usage string

--- 2023-03-29-homework07/library.c
+++ 2023-03-29-homework07/library.c
@@ -7,4 +7,4 @@
 #define O C*s){for
 #define I e=b=s;*b
 #define T(x) {C w[1<<7]={0};for(b=from;*b;b++)(*b)[w]=x;for(I;b++
-C*b,*e,*p,t;V str_lower(O(;(*s=tolower(*s));s++);}V str_upper(O(;(*s=U);s++);}V str_title(O(*s=U;*s;*s=isalpha(*e)?tolower(*s):U)e=s++;}V str_chomp(C*s){s+=strlen(s)-1;*s=*s-10?*s:0;}V str_strip(O(I&&isspace(*b);)b++;for(e=b+strlen(b)-1;isspace(*e);)e--;for(e++;b<e;)*s++=*b++;*s=0;}V str_reverse(O(I;)b++;for(b--;e<b;*e++=*b,*b--=t)t=*e;}V str_delete(C*s,C*from)T(1))if(!(*b)[w])*e++=*b;*e=0;}V str_translate(C*s,C*from,C*to)T(*to++),e++)*e=(*b)[w]?:*e;*e=0;}
+C*b,*e,*p,t;V str_lower(O(;(*s=tolower(*s));s++);}V str_upper(O(;(*s=U);s++);}V str_title(O(*s=U;*s;*s=isalpha(*e)?tolower(*s):U)e=s++;}V str_chomp(C*s){s+=strlen(s)-1;*s=*s-10?*s:0;}V str_strip(O(I&&isspace(*b);)b++;for(e=b+strlen(b)-1;*b&&isspace(*e);)e--;for(e++;b<e;)*s++=*b++;*s=0;}V str_reverse(O(I;)b++;for(b--;e<b;*e++=*b,*b--=t)t=*e;}V str_delete(C*s,C*from)T(1))if(!(*b)[w])*e++=*b;*e=0;}V str_translate(C*s,C*from,C*to)T(*to++),e++)*e=(*b)[w]?:*e;*e=0;}
--- 2023-03-29-homework07/main.c
+++ 2023-03-29-homework07/main.c
@@ -2,4 +2,4 @@
 #include<stdio.h>
 #include<string.h>
 #define H char
-H*P,*s,*t,*K="hsrlutd",*A;int f=1,b,i;int u(int s){puts("usage");exit(s);}void N(H*s,H*t){H l[BUFSIZ];for(void(*p[])(H*)={str_chomp,str_strip,str_reverse,str_lower,str_upper,str_title};fgets(l,BUFSIZ,stdin);puts(l)){if(f>>6)str_delete(l,s);if(s&&t)str_translate(l,s,t);for(i=0;i<6;i++)if(f&1<<i)p[i](l);}}int main(int C,H*V[]){for(P=*V++;--C;)if(*(A=*V++)-45)if(s){if(t)u(1);t=A;}else s=A;else f|=1<<((b=strchr(K,A[1])-K)>0?b:b?u(1):u(0));N(s,t);return 0;}
+H*P,*s,*t,*K="hrltusaged",*A;int f=1,b,i;int u(int s){puts(K);exit(s);}void N(H*s,H*t){H l[BUFSIZ];for(void(*p[])(H*)={str_chomp,str_reverse,str_lower,str_title,str_upper,str_strip};fgets(l,BUFSIZ,stdin);puts(l)){if(f&1<<9)str_delete(l,s);if(s&&t)str_translate(l,s,t);for(i=0;i<6;i++)if(f&1<<i)p[i](l);}}int main(int C,H*V[]){for(P=*V++;--C;)if(*(A=*V++)-45)if(s){if(t)u(1);t=A;}else s=A;else f|=1<<((b=strchr(K,A[1])-K)>0?b:b?u(1):u(0));N(s,t);}
