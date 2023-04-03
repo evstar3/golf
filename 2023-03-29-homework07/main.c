304fff05e6aa277d27f63f8de9deab544a9bce74
Evan Day <evanday19@gmail.com>
2023-04-03 09:32:35 -0400

    shortened main

--- 2023-03-29-homework07/main.c
+++ 2023-03-29-homework07/main.c
@@ -1,5 +1,4 @@
 #include"str.h"
 #include<stdio.h>
 #include<string.h>
-#define H char
-H*s,*t,*K="hrltusaged",*A;int f=1,i;int u(){puts(K);exit(!*A);}void N(H*t){H l[99];for(void(*p[])(H*)={str_chomp,str_reverse,str_lower,str_title,str_upper,str_strip};fgets(l,99,stdin);puts(l)){if(f&1<<9)str_delete(l,s);if(s&&t)str_translate(l,s,t);for(i=0;i<6;i++)if(f&1<<i)p[i](l);}}int main(int C,H*V[]){for(;--C;)if(*(A=*++V)-45)if(s){if(t)u();t=A;}else s=A;else f|=1<<(strchr(K,A[1])-K?:u());N(t);}
+char*s,*t,*K="hrltusaged",*A;int f=1,i;int u(){puts(K);exit(!*A);}void N(char*t){char l[99];for(void(*p[])(char*)={str_chomp,str_reverse,str_lower,str_title,str_upper,str_strip};fgets(l,99,stdin);puts(l)){if(f>>9)str_delete(l,s);if(s&&t)str_translate(l,s,t);for(i=0;i<6;i++)p[f&1<<i?i:0](l);}}int main(int C,char*V[]){for(;--C;)if(*(A=*++V)-45)t=(s=s?:A)?A:t;else f|=1<<(strchr(K,A[1])-K?:u());N(t);}
